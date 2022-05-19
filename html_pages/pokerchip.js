3c14cec8e1b155518e8b6013e11d38363fbc0c10
mitalrs <mitalsapkale1999@gmail.com>
2022-05-19 23:46:39 +0530

    update poker chip page

---
+++ html_pages/pokerchip.js
@@ -0,0 +1,5 @@
+function white(){
+    document.getElementById('wt').className = 'pokerwhite';
+    document.getElementById('bet').className = 'betc';
+    document.getElementById('getplay').className = 'getplayc';
+}

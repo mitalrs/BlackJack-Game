3aa785f295847b4863af661e30c5d99d974ff8a2
mitalrs <mitalsapkale1999@gmail.com>
2022-05-22 15:38:28 +0530

    update poker-chip pg

--- html_pages/pokerchip.js
+++ html_pages/pokerchip.js
@@ -3,3 +3,12 @@ function white(){
     document.getElementById('bet').className = 'betc';
     document.getElementById('getplay').className = 'getplayc';
 }
+function cashout(){
+    document.getElementById('imgdiv').className = 'imgdiv';
+    document.getElementById('exitflashdiv').className = 'exitflashdiv';
+    document.getElementById('p1').className = 'p1';
+    document.getElementById('p2').className = 'p2';
+    document.getElementById('p3').className = 'p3';
+    document.getElementById('p4').className = 'p4';
+    document.getElementById('p5').className = 'p5';
+}

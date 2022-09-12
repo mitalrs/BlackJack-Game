025da86724d89f52eb4f708670efaeeb9e01406a
mitalrs <mitalsapkale1999@gmail.com>
2022-09-12 17:58:33 +0530

    js update

--- pokerchip_pages/Game_page/gamepg.js
+++ pokerchip_pages/Game_page/gamepg.js
@@ -11,6 +11,8 @@ var conHit = true;
 
 window.onload = function(){
     buildDeck();
+    shuffleDeck();
+    startGame();
 }
 
 function buildDeck(){
@@ -26,5 +28,54 @@ function buildDeck(){
             deck.push(values[j]+ "-" + types[i]);
         }
     }
+    // console.log(deck);
 }
 
+function shuffleDeck(){
+    for(let i=0;i<deck.length;i++)
+    {
+        let j=Math.floor(Math.random()*deck.length);
+        let temp=deck[i];
+        deck[i]=deck[j];
+        deck[j]=temp;
+    }
+    console.log(deck);
+}
+
+function startGame(){
+    hidden=deck.pop();
+    dealerSum+= getValue(hidden);
+    dealerAceCount+=checkAce(hidden);
+
+    // console.log(hidden);
+    // console.log(dealerSum);
+    while(dealerSum < 17){
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.scr = "./PNG/"+card+".png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
+    }
+    console.log(dealerSum);
+}
+
+function getValue(card){
+    let data = card.split("-");   //"4-c" png name
+    let value = data[0];
+
+    if(isNaN(value)){
+        if(value == "A"){
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(value);
+}
+
+function checkAce(card){
+    if(card[0]=="A"){
+        return 1;
+    }
+    return 0;
+}
